9cc7e463704d11a0a5370d25f81819b3875e0e7d
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-10-21 21:14:08 -0700

    added and started working on the game manager

--- Assets/Scripts/Game Manager/GameManager.cs
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance;
+    private GameObject[] puzzlePieces;
+    private Sprite[] puzzleImages;
+    private Vector3 screenPosToAnimate;
+    private PuzzlePiece PieceToAnimate;
+    private int toAnimateRow, toAnimateCol;
+    private float animSpeed = 10f;
+    private int puzzleIndex;
+    private GameState gameState;
 
+    private void Awake() {
+        MakeSingleton();
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void MakeSingleton(){
+        if(instance !=null){
+            Destroy(gameObject);
+        }else{
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void SetPuzzleIndex(int puzzleIndex){
+        this.puzzleIndex = puzzleIndex;
     }
 }
