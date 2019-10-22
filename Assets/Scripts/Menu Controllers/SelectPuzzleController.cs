3edf07063f42c6d203b425e84fe625648ff0b4b4
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-10-21 20:22:18 -0700

    added the menu controllers that make the buttons somewhat functional

---
+++ Assets/Scripts/Menu Controllers/SelectPuzzleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectPuzzleController : MonoBehaviour
+{
+    public void SelectPuzzle(){
+        string[] name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name.Split();
+        int index = int.Parse(name[1]);
+
+        switch (index)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void LoadMainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+}
