3ee654e8fa03aa06b803d09bdc65d99ec3fd0799
Maxx Rodriguez <maxxrdrgz@gmail.com>
2019-10-21 20:53:50 -0700

    setup the helper classes

---
+++ Assets/Scripts/Game Variables/GameVariables.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameVariables
+{
+    public static int MaxRows = 4;
+    public static int MaxCols = 4;
+    public static int MaxSize = MaxRows * MaxCols;
+}
+
+public enum GameState {
+    Playing,
+    Animating,
+    End
+}
---
+++ Assets/Scripts/Puzzle/PuzzlePiece.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece
+{
+    public static int CurrentRow {get; set;}
+    public static int CurrentCol {get; set;}
+    public static int OriginalRow {get; set;}
+    public static int OriginalCol {get; set;}
+    public static GameObject GameObject {get; set;}
+}
